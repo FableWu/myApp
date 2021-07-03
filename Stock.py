97513bb7e72a8c9049f12c2da3373545d246a53b
FableWu <2437738024@qq.com>
2021-07-04 00:25:00 +0800

    set secrets

--- Stock.py
+++ Stock.py
@@ -12,6 +12,9 @@ minus60 = (datetime.today() - timedelta(days=100)).strftime('%Y%m%d')
 
 starttime = datetime.now()
 
+st.write("username:", st.secrets["username"])
+st.write("password:", st.secrets["password"])
+
 
 @st.cache()
 def filter_alldata():
